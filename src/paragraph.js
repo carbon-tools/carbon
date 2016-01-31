3ef18ef09c27b39e2626ea956557b88f37db7f64
Mohammad Khatib <mkhatib727@gmail.com>
2016-01-31 12:29:38 -0800

    Only add contenteditable in editmode.

--- src/paragraph.js
+++ src/paragraph.js
@@ -62,7 +62,6 @@ var Paragraph = function(optParams) {
    */
   this.dom = document.createElement(this.paragraphType);
   this.dom.setAttribute('name', this.name);
-  this.dom.setAttribute('contenteditable', true);
 
   this.setText(params.text);
 
@@ -598,6 +597,7 @@ Paragraph.prototype.render = function(element, options) {
     Component.prototype.render.call(this, element, options);
 
     if (this.editMode) {
+      this.dom.setAttribute('contenteditable', true);
       if (this.placeholderText) {
         this.dom.setAttribute('placeholder', this.placeholderText);
       } else if (!this.text.length) {
