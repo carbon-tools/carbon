d7ac24d31f36972022968c8b30e4710804434693
Saad Shahd <dev.saad.shahd@gmail.com>
2016-02-05 10:33:03 +0200

    Fix karma.conf.js file indentation to match editorconfig options

--- karma.conf.js
+++ karma.conf.js
@@ -2,65 +2,65 @@
 // http://karma-runner.github.io/0.12/config/configuration-file.html
 
 module.exports = function(config) {
-    'use strict';
+  'use strict';
 
-    config.set({
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-        // base path, that will be used to resolve files and exclude
-        basePath: './',
+    // base path, that will be used to resolve files and exclude
+    basePath: './',
 
-        // testing framework to use (jasmine/mocha/qunit/...)
-        frameworks: ['jasmine'],
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
 
-        // list of files / patterns to load in the browser
-        files: [
-            'build/editor.spec.js'
-        ],
+    // list of files / patterns to load in the browser
+    files: [
+      'build/editor.spec.js'
+    ],
 
-        // list of files / patterns to exclude
-        exclude: [],
+    // list of files / patterns to exclude
+    exclude: [],
 
-        // web server port
-        port: 8080,
+    // web server port
+    port: 8080,
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
-        browsers: [
-            'PhantomJS'
-        ],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+      'PhantomJS'
+    ],
 
-        // Which plugins to enable
-        plugins: [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-phantomjs-launcher',
-            'karma-jasmine',
-            'karma-coverage'
-        ],
+    // Which plugins to enable
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-coverage'
+    ],
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false,
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
-        colors: true,
+    colors: true,
 
-        // level of logging
-        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: config.LOG_INFO
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO
 
-        // Uncomment the following lines if you are using grunt's server to run the tests
-        // proxies: {
-        //   '/': 'http://localhost:9000/'
-        // },
-        // URL root prevent conflicts with the site root
-        // urlRoot: '_karma_'
-    });
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+  });
 };
