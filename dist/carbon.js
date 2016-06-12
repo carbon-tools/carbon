693e6d1c2f0eb6dd6db29c89bf31dfece77a6bfd
Mohammad Khatib <mkhatib727@gmail.com>
2016-06-11 18:07:53 -0700

    Fix deleting non-text components. (#90) (#96)

--- dist/carbon.js
+++ dist/carbon.js
@@ -1264,6 +1264,10 @@ Editor.prototype.handleInputEvent = function() {
   var modelText = component.text;
   var modelLength = component.getLength();
 
+  if (!(component instanceof Paragraph)) {
+    return;
+  }
+
   var domText = Utils.getTextFromElement(component.dom);
   var domLength = domText.length;
 
